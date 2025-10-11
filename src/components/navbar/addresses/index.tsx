743ddeace19a7fc481fc07778fac863ee11d049a
Luis Alvarez <mrluisfeer@gmail.com>
2025-10-11 16:28:21 -0600

    feat: implement Addresses component with dialog for managing saved addresses

--- src/components/navbar/addresses/index.tsx
+++ src/components/navbar/addresses/index.tsx
@@ -1,3 +1,34 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog';
+import { MapPinHouseIcon } from 'lucide-react';
+
 export const Addresses = () => {
-    return <div>Addresses</div>;
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant={'default'} size={'icon'} className="relative">
+                    <MapPinHouseIcon />
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Manage your Addresses</DialogTitle>
+                    <DialogDescription>
+                        You can add, edit, or remove your saved addresses here.
+                        These addresses will be used for delivery and billing
+                        purposes.
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    );
 };
