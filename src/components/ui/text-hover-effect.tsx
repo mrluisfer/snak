98d7829bd3f024356a0dfaa70c0b2140e409fb8f
Luis Alvarez <mrluisfeer@gmail.com>
2025-10-12 16:51:34 -0600

    fix: update stroke class for TextHoverEffect component to improve dark mode visibility

--- src/components/ui/text-hover-effect.tsx
+++ src/components/ui/text-hover-effect.tsx
@@ -95,7 +95,7 @@ export const TextHoverEffect = ({
                 textAnchor="middle"
                 dominantBaseline="middle"
                 strokeWidth="0.3"
-                className="fill-transparent stroke-neutral-200 font-[helvetica] text-7xl font-bold dark:stroke-neutral-800"
+                className="dark:stroke-accent fill-transparent stroke-neutral-200 font-[helvetica] text-7xl font-bold"
                 style={{ opacity: hovered ? 0.7 : 0 }}
             >
                 {text}
@@ -106,7 +106,7 @@ export const TextHoverEffect = ({
                 textAnchor="middle"
                 dominantBaseline="middle"
                 strokeWidth="0.3"
-                className="fill-transparent stroke-neutral-200 font-[helvetica] text-7xl font-bold dark:stroke-neutral-800"
+                className="dark:stroke-accent fill-transparent stroke-neutral-200 font-[helvetica] text-7xl font-bold"
                 initial={{ strokeDashoffset: 1000, strokeDasharray: 1000 }}
                 animate={{
                     strokeDashoffset: 0,
