05177a155094a7145d7248b362445162c11604db
Luis Alvarez <mrluisfeer@gmail.com>
2025-10-13 21:36:49 -0600

    feat: enhance Addresses component with loading and error states, and user session management

--- src/components/navbar/addresses/index.tsx
+++ src/components/navbar/addresses/index.tsx
@@ -9,11 +9,45 @@ import {
     DialogTitle,
     DialogTrigger,
 } from '@/components/ui/dialog';
-import { MapPinHouseIcon } from 'lucide-react';
+import { Spinner } from '@/components/ui/spinner';
+import { authClient } from '@/lib/auth-client';
+import { BugIcon, MapPinHouseIcon, UserIcon } from 'lucide-react';
+import Link from 'next/link';
 import { AddNewAddress } from './add-new-address';
 import { ListAllAddresses } from './list-all-addresses';
 
 export const Addresses = () => {
+    const { data, error, isPending } = authClient.useSession();
+
+    if (isPending) {
+        return (
+            <div className="bg-accent flex items-center justify-center gap-2 rounded-lg p-1">
+                <Spinner />
+                <p>Loading...</p>
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div className="flex items-center justify-center gap-2 rounded-lg bg-red-500/10 p-1 text-red-500">
+                <BugIcon />
+                <p>Error loading addresses</p>
+            </div>
+        );
+    }
+
+    if (!data) {
+        return (
+            <Button asChild variant={'outline'} size={'sm'}>
+                <Link href={'/auth/login'}>
+                    <UserIcon className="text-primary" />
+                    <p>Log in to manage your addresses.</p>
+                </Link>
+            </Button>
+        );
+    }
+
     return (
         <Dialog>
             <DialogTrigger asChild>
